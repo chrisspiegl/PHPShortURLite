72faeb4bce01ee06a3bebac8773c9c50a792aa84
Christoph Spiegl <chris@chrissp.com>
2012-08-09 00:45:17 +0200

    Added 'http://' to the 'SHORT_URL'. Added some more comments.

--- config.example.php
+++ config.example.php
@@ -1,6 +1,6 @@
 <?php
-define('SHORT_URL', 'shortener.imac:8888');		// Your shortener URL
-define('DEFAULT_URL', 'http://cspiegl.com');	// Your default redirect site
+define('SHORT_URL', 'http://s.cspiegl.com');	// Your shortener URL (no trailing '/')
+define('DEFAULT_URL', 'http://cspiegl.com');	// Your default redirect site (no trailing '/')
 
 define('AUTH', true);				// true if authentication is needed, false if everybody can shorten.
 define('CACHE', true);				// Cache the original link in a file to prevent sql requests
@@ -18,9 +18,9 @@ $API_AUTH = array('cspiegl'=>'27459b72456f8fd37e7080bb7fcbe6884c54697f');
 // Define some folders for the process
 define('DOC_ROOT', dirname(__FILE__));
 define('DB_FILE_NAME', DOC_ROOT . '/data/db.sqlite');
-define('CACHE_DIR', DOC_ROOT . '/data/cache');
-define('TRACK_DIR', DOC_ROOT . '/data/log');
-define('TRACK_FILE_NAME', date("Y_m") . '_access.log');
+define('CACHE_DIR', DOC_ROOT . '/data/cache');	// (no trailing '/')
+define('TRACK_DIR', DOC_ROOT . '/data/log');	// (no trailing '/')
+define('TRACK_FILE_NAME', date("Y_m") . '_access.log');	// Creates one logfile for each month of the year
 
 // Stop editing here!
 require_once(DOC_ROOT . '/functions.php');
